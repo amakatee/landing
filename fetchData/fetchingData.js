abb3f6a20b11a138e1283414e4fb92fd7127f087
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2022-12-10 17:17:35 +0300

    url

--- fetchData/fetchingData.js
+++ fetchData/fetchingData.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
-const BASE_URL='http://localhost:3000/api'
+
 
 export default axios.create({
    
 })
 const axiosBase = axios.create({
-    baseURL:'http://localhost:3000/api',
+    baseURL:`${process.env.BASE_URL}/api`,
     credentials: "include"
   });
 //navbar
